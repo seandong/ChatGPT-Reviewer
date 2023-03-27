ea123df1e5c204a3ca6ec896cc0171632e242a7d
seandong <sindon@gmail.com>
2023-03-27 15:23:35 +0800

    Update completion.py

--- app/completion.py
+++ app/completion.py
@@ -7,25 +7,11 @@ import openai
 import tiktoken
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-system_prompt = '''Please review the code below and identify any syntax or logical errors, suggest ways to refactor and improve code quality, enhance performance, address security
-concerns, and align with best practices. Provide specific examples for each area and limit your recommendations to three per category.
-Use the following response format, keeping the section headings as-is, and provide your feedback. Use bullet points for each response. The provided examples are for
-illustration purposes only and should not be repeated. please answer in Chinese.
-**Syntax and logical errors (example)**: 
-- Incorrect indentation on line 12
-- Missing closing parenthesis on line 23
-**Code refactoring and quality (example)**: 
-- Replace multiple if-else statements with a switch case for readability
-- Extract repetitive code into separate functions
-**Performance optimization (example)**: 
-- Use a more efficient sorting algorithm to reduce time complexity
-- Cache results of expensive operations for reuse
-**Security vulnerabilities (example)**: 
-- Sanitize user input to prevent SQL injection attacks
-- Use prepared statements for database queries
-**Best practices (example)**: 
-- Add meaningful comments and documentation to explain the code
-- Follow consistent naming conventions for variables and functions
+system_prompt = '''As a tech reviewer, please provide an code review of the following changes made and pay close attention to the following:
+* Identify any syntax or logical errors, suggest ways to refactor and improve code quality.
+* Avoid providing unnecessary explanations or summaries that may delay the review process.
+* No need for thanking in the review message.
+* Answer in Chinese.
 '''
 
 
@@ -99,7 +85,8 @@ class OpenAIClient:
                 completion_text += event["choices"][0]["text"]
         return completion_text
 
-    def get_pr_prompt(self, title, body, changes) -> str:
+    def get_pr_prompt(self, title, body, 
+                      s) -> str:
         '''Generate a prompt for a PR review'''
         prompt = f'''Here are the changes for this pull request:
 Changes:
