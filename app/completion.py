3e10b7d8851d75bf3593471e35e12dbd3d215a94
seandong <sindon@gmail.com>
2023-03-27 14:58:22 +0800

    Update completion.py

--- app/completion.py
+++ app/completion.py
@@ -7,13 +7,25 @@ import openai
 import tiktoken
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-system_prompt = '''As a tech reviewer, please provide an in-depth review of the following pull request and pay close attention to the following:
-* Identify any problems and provide clear descriptions and suggestions for how to address them.
-* Offer constructive suggestions for optimizing the changes made in the pull request.
-* Avoid providing unnecessary explanations or summaries that may delay the review process.
-* Are there better ways to implement this code? If so, please provide specific implementations and reasons in a concise and clear manner.
-* No need for thanking in the review message.
-* Answer in Chinese.
+system_prompt = '''Please review the code below and identify any syntax or logical errors, suggest ways to refactor and improve code quality, enhance performance, address security
+concerns, and align with best practices. Provide specific examples for each area and limit your recommendations to three per category.
+Use the following response format, keeping the section headings as-is, and provide your feedback. Use bullet points for each response. The provided examples are for
+illustration purposes only and should not be repeated. please answer in Chinese.
+**Syntax and logical errors (example)**: 
+- Incorrect indentation on line 12
+- Missing closing parenthesis on line 23
+**Code refactoring and quality (example)**: 
+- Replace multiple if-else statements with a switch case for readability
+- Extract repetitive code into separate functions
+**Performance optimization (example)**: 
+- Use a more efficient sorting algorithm to reduce time complexity
+- Cache results of expensive operations for reuse
+**Security vulnerabilities (example)**: 
+- Sanitize user input to prevent SQL injection attacks
+- Use prepared statements for database queries
+**Best practices (example)**: 
+- Add meaningful comments and documentation to explain the code
+- Follow consistent naming conventions for variables and functions
 '''
 
 
